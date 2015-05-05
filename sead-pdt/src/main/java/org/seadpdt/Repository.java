17bf81b6b49f18060b6ec8710405aa3cae6c693b
Scott McCaulay <scottmccaulay@gmail.com>
2015-05-04 20:27:30 -0400

    Added Repository class, returning valid JSON
    Jackson JSON processor does not allow the @ character so far in JSON-LD,
    need to find another way to include those

---
+++ sead-pdt/src/main/java/org/seadpdt/Repository.java
@@ -0,0 +1,49 @@
+package org.seadpdt;
+
+public class Repository {
+	 
+	String context;
+	String type;
+	String orgidentifier;
+	String repositoryName;
+ 
+	public String getContext() {
+		return context;
+	}
+ 
+	public void setContext(String context) {
+		this.context = context;
+	}
+ 
+	public String getType() {
+		return type;
+	}
+ 
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getOrgIdentifier() {
+		return orgidentifier;
+	}
+ 
+	public void setOrgIdentifier(String orgidentifier) {
+		this.orgidentifier = orgidentifier;
+	}
+ 
+	public String getRepositoryName() {
+		return repositoryName;
+	}
+ 
+	public void setRepositoryName(String repositoryName) {
+		this.repositoryName = repositoryName;
+	}	
+ 
+	@Override
+	public String toString() {
+		return "Repository [context=" + context + ", type=" + type + 
+				", orgidentifier" + orgidentifier + ", repositoryName" +
+				repositoryName + "]";
+	}
+	
+}
--- sead-pdt/src/main/java/org/seadpdt/SeadPDT.java
+++ sead-pdt/src/main/java/org/seadpdt/SeadPDT.java
@@ -2,16 +2,26 @@ package org.seadpdt;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+import org.seadpdt.Repository;
+
+@Path("/PDT")
 public class SeadPDT {
  
 	 @GET
-	 @Produces(MediaType.TEXT_PLAIN)
-	 public String ping() {
-		 return "Hey, This is Jersey JAX-RS !";
+	 @Path("/repo")
+	 @Produces(MediaType.APPLICATION_JSON)
+	 public Repository getRepo(@PathParam("varX") String varX,
+			    @PathParam("varY") String varY) {	 
+		 Repository repository = new Repository();
+		 repository.setContext("http://re3data.org/");
+		 repository.setType("repository"); 
+		 repository.setOrgIdentifier("https://www.ideals.illinois.edu");
+		 repository.setRepositoryName("IDEALS"); 
+		 return repository;
 	 }
  
 }
