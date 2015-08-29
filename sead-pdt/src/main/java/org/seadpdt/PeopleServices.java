361e40d1101cae90429c59e4af0c23db8c62ba43
Scott McCaulay <scottmccaulay@gmail.com>
2015-08-29 13:21:29 -0400

    updated People and Repo APIs to match shim API
    Incorporates much of Jim's code from the seadcp implementations of the
    shim, which were improvements over the previous implementations here

--- sead-pdt/src/main/java/org/seadpdt/PeopleServices.java
+++ sead-pdt/src/main/java/org/seadpdt/PeopleServices.java
@@ -1,20 +1,34 @@
 package org.seadpdt;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.bson.Document;
-import org.json.JSONObject;
+import org.json.JSONArray;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.ClientResponse.Status;
+import com.sun.jersey.api.client.WebResource;
 
 @Path("/people")
 
@@ -27,55 +41,130 @@ public class PeopleServices {
 	MongoClient mongoClient = new MongoClient();
 	MongoDatabase db = mongoClient.getDatabase(DBname);
 	MongoCollection<Document> collection = db.getCollection(collectionName);	
+	CacheControl control = new CacheControl();	
 	
-	 @POST
-	 @Path("/add")
-	 @Consumes(MediaType.APPLICATION_JSON)
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public String addPerson(String personData)  {	 
-		JSONObject xmlJSONObj = new JSONObject(personData);			
-		Document doc = Document.parse(xmlJSONObj.toString());
-		collection.drop(); // needs to drop by ID, not drop all
-		collection.insertOne(doc);
-		mongoClient.close();		
-		return "success";
-	 }	
-	 	 
-	 @GET
-	 @Path("/list")
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public FindIterable<Document> listPeople() {	
-		 FindIterable<Document> iterable = db.getCollection(collectionName).find();
-		 return iterable;
-	 }		 
-	 
-	@GET
-	@Path("/byid")
-	@Produces(MediaType.APPLICATION_JSON)
-	 public FindIterable<Document> getPeople(
-		@QueryParam("id") String repID) {
-		FindIterable<Document> iterable = db.getCollection(collectionName).find();
-		return iterable;		
-	}	 	 
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response registerPerson(String personString) {
 
-	 @POST
-	 @Path("/delete")
-	 @Consumes(MediaType.APPLICATION_JSON)
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public String deletePerson(String personData)  {	 
-		JSONObject xmlJSONObj = new JSONObject(personData);			
-		Document doc = Document.parse(xmlJSONObj.toString());
-		collection.drop(); // needs to drop by ID, not drop all
-		mongoClient.close();		
-		return "success";
-	 }		
-	
-	 @GET
-	 @Path("/orcid")
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public String listORCID(
-				@QueryParam("id") String orcidID)  {	
-		 return ORCIDcalls.getORCID(orcidID);
-	 }			 
-	
+			BasicDBObject person = (BasicDBObject) JSON.parse(personString);
+
+			String newID = (String) person.get("identifier");
+			FindIterable<Document> iter = collection.find(new Document(
+					"identifier", newID));
+			if (iter.iterator().hasNext()) {
+				return Response.status(Status.CONFLICT).build();
+			} else {
+				if (person.get("provider").equals("ORCID")) {
+					String orcidID = (String) person.get("identifier");
+					String profile = null;
+					try {
+						profile = getOrcidProfile(orcidID);
+					} catch (RuntimeException r) {
+						return Response
+								.serverError()
+								.entity(new BasicDBObject("failure",
+										"Provider call failed with status: "
+												+ r.getMessage())).build();
+					}
+					collection.insertOne(Document.parse(profile));
+					URI resource = null;
+					try {
+						resource = new URI("./" + newID);
+					} catch (URISyntaxException e) {
+						// Should not happen given simple ids
+						e.printStackTrace();
+					}
+					return Response.created(resource)
+							.entity(new Document("identifier", newID)).build();
+				} else {
+					return Response.status(Status.BAD_REQUEST).entity(new BasicDBObject("Failure", "Provider " + person.get("provider") + " not supported")).build();
+				}
+			}
+		}
+
+		@GET
+		@Path("/")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response getPeopleList() {
+			FindIterable<Document> iter = collection.find();
+			iter.projection(new Document("orcid-profile.orcid-identifier.path", 1).append(
+					"orcid-profile.orcid-bio.personal-details.given-names", 1).append("orcid-profile.orcid-bio.personal-details.family-name", 1).append("_id", 0));
+			MongoCursor<Document> cursor = iter.iterator();
+			JSONArray array = new JSONArray();
+			while (cursor.hasNext()) {
+				array.put(cursor.next().toJson());
+			}
+			return Response.ok(array.toString()).cacheControl(control).build();
+
+		}
+
+		@GET
+		@Path("/{id}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response getPersonProfile(@PathParam("id") String id) {
+			FindIterable<Document> iter = collection.find(new Document(
+					"orcid-profile.orcid-identifier.path", id));
+			Document document = iter.first();
+			document.remove("_id");
+			return Response.ok(document.toJson()).cacheControl(control).build();
+		}
+
+		@PUT
+		@Path("/{id}")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response updatePersonProfile(@PathParam("id") String id) {
+			FindIterable<Document> iter = collection.find(new Document(
+					"orcid-profile.orcid-identifier.path", id));
+
+			if (iter.iterator().hasNext()) {
+				String orcidID = (String) iter.first().get("orcid-profile.orcid-identifier.path");
+				String profile = null;
+				try {
+					profile = getOrcidProfile(orcidID);
+				} catch (RuntimeException r) {
+					return Response
+							.serverError()
+							.entity(new BasicDBObject("failure",
+									"Provider call failed with status: "
+											+ r.getMessage())).build();
+				}
+			
+				
+				collection.replaceOne(new Document(
+						"orcid-profile.orcid-identifier.path", id), Document.parse(profile));
+				return Response.status(Status.OK).build();
+
+			} else {
+				return Response.status(Status.NOT_FOUND).build();
+
+			}
+		}
+
+		@DELETE
+		@Path("/{id}")
+		public Response unregisterPerson(@PathParam("id") String id) {
+			collection.deleteOne(new Document("orcid-profile.orcid-identifier.path", id));
+			return Response.status(Status.OK).build();
+		}
+
+		private String getOrcidProfile(String id) {
+
+			Client client = Client.create();
+			WebResource webResource = client.resource("http://pub.orcid.org/v1.1/"
+					+ id + "/orcid-profile");
+
+			ClientResponse response = webResource.accept("application/orcid+json")
+					.get(ClientResponse.class);
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("" + response.getStatus());
+			}
+
+			return response.getEntity(String.class);
+		}
+	 
+	 
 }
--- sead-pdt/src/main/java/org/seadpdt/RepoServices.java
+++ sead-pdt/src/main/java/org/seadpdt/RepoServices.java
@@ -1,20 +1,30 @@
 package org.seadpdt;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.bson.Document;
-import org.json.JSONObject;
+import org.json.JSONArray;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import com.sun.jersey.api.client.ClientResponse.Status;
 
 @Path("/repositories")
 public class RepoServices {
@@ -25,48 +35,91 @@ public class RepoServices {
 	
 	MongoClient mongoClient = new MongoClient();
 	MongoDatabase db = mongoClient.getDatabase(DBname);
-	MongoCollection<Document> collection = db.getCollection(collectionName);		
-		
-	 @POST
-	 @Path("/add")
-	 @Consumes(MediaType.APPLICATION_JSON)
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public String addRepository(String RepositoryData)  {	 
-		JSONObject xmlJSONObj = new JSONObject(RepositoryData);			
-		Document doc = Document.parse(xmlJSONObj.toString());
-		collection.drop(); // needs to drop by ID, not drop all
-		collection.insertOne(doc);
-		mongoClient.close();		
-		return "success";
-	 }	
-	 	 
-	 @GET
-	 @Path("/list")
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public FindIterable<Document> listRepositories() {	
-		 FindIterable<Document> iterable = db.getCollection(collectionName).find();
-		 return iterable;
-	 }		 
-	 
-	@GET
-	@Path("/byid")
-	@Produces(MediaType.APPLICATION_JSON)
-	 public FindIterable<Document> getRepository(
-		@QueryParam("id") String repID) {
-		FindIterable<Document> iterable = db.getCollection(collectionName).find();
-		return iterable;		
-	}	 	 
-
-	 @POST
-	 @Path("/delete")
-	 @Consumes(MediaType.APPLICATION_JSON)
-	 @Produces(MediaType.APPLICATION_JSON)
-	 public String deleteRepository(String RepositoryData)  {	 
-		JSONObject xmlJSONObj = new JSONObject(RepositoryData);			
-		Document doc = Document.parse(xmlJSONObj.toString());
-		collection.drop(); // needs to drop by ID, not drop all
-		mongoClient.close();		
-		return "success";
-	 }		
+	MongoCollection<Document> collection = db.getCollection(collectionName);			
 	
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response registerRepository(String profileString) {
+			BasicDBObject profile = (BasicDBObject) JSON.parse(profileString);
+			String newID = (String) profile.get("orgidentifier");
+			FindIterable<Document> iter = collection.find(new Document(
+					"orgidentifier", newID));
+			if (iter.iterator().hasNext()) {
+				return Response.status(Status.CONFLICT).build();
+			} else {
+				collection
+						.insertOne(Document.parse(profile.toString()));
+				URI resource = null;
+				try {
+					resource = new URI("./" + newID);
+				} catch (URISyntaxException e) {
+					// Should not happen given simple ids
+					e.printStackTrace();
+				}
+				return Response.created(resource).entity(new Document("orgidentifier", newID)).build();
+			}
+		}
+
+		@GET
+		@Path("/")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response getRepositoryList() {
+			FindIterable<Document> iter = collection.find();
+			iter.projection(new Document("orgidentifier", 1).append("repositoryURL", 1).append("_id", 0));
+			MongoCursor<Document> cursor = iter.iterator();
+			JSONArray array = new JSONArray();
+			while(cursor.hasNext()) {
+				array.put(cursor.next().toJson());
+			}
+			return Response.ok(array.toString()).build();
+
+		}
+
+		@GET
+		@Path("/{id}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response getRepositoryProfile(@PathParam("id") String id) {
+			FindIterable<Document> iter = collection.find(new Document(
+					"orgidentifier", id));
+			Document document = iter.first();
+			document.remove("_id");
+			return Response.ok(document.toJson()).build();
+		}
+
+		@PUT
+		@Path("/{id}")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response setRepositoryProfile(@PathParam("id") String id,
+				String profile) {
+			FindIterable<Document> iter = collection.find(new Document(
+					"orgidentifier", id));
+			
+			if (iter.iterator().hasNext()) {
+
+			Document document = Document.parse(profile);
+			collection.replaceOne(new Document("orgidentifier", id),document);
+			return Response.status(Status.OK).build();
+
+			} else {
+				return Response.status(Status.NOT_FOUND).build();
+				
+			}
+		}
+
+		@DELETE
+		@Path("/{id}")
+		public Response unregisterRepository(@PathParam("id") String id) {
+			collection.deleteOne(new Document("orgidentifier", id));
+			return Response.status(Status.OK).build();
+		}
+
+		@GET
+		@Path("/researchobjects")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response getROsByRepository() {
+			return Response.status(Status.NOT_IMPLEMENTED).build();
+		};	 
+	 
 }
