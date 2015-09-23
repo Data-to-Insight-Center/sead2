f5a8f3275670e531034e4771514ded2b5472edf7
Yu Luo <luoyu357@gmail.com>
2015-09-23 15:26:44 -0400

    draft SDA Agent without Landing Page

---
+++ sda-agent/src/main/java/SDA_Agent/Calls/CallDaemons.java
@@ -0,0 +1,44 @@
+package Calls;
+
+
+
+import org.apache.log4j.Logger;
+
+public class CallDaemons {
+	
+	private Thread[] callDaemons;
+	private int numberofcallDaemons;
+	private Logger log;
+	
+	
+	public CallDaemons(String propertiesPath, CallConfig callConfig){
+		
+		this.numberofcallDaemons = callConfig.getDaemon();
+		
+		this.callDaemons = new Thread[this.numberofcallDaemons];
+		
+		SynchronizedReceiverRunnable srr;
+		
+		try {
+			srr = new SynchronizedReceiverRunnable(propertiesPath);
+			for (int i = 0; i < this.numberofcallDaemons; i++){
+				this.callDaemons[i] = new Thread(srr);
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void start() throws IllegalMonitorStateException{
+		log = Logger.getLogger(CallDaemons.class);
+		
+		for (int i = 0; i < this.numberofcallDaemons; i++){
+			this.callDaemons[i].start();
+			//log.info("Call Daemon [" + i +"] Started");
+		
+		}
+	}
+
+}
---
+++ sda-agent/src/main/java/SDA_Agent/Calls/SynchronizedReceiverRunnable.java
@@ -0,0 +1,128 @@
+package Calls;
+
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import APIcalls.NewOREmap;
+import APIcalls.Shimcalls;
+import Driver.DummySDA;
+import Driver.FileManager;
+import Driver.ZipDirectory;
+import Engine.DOI;
+import Engine.PropertiesReader;
+import Engine.SFTP;
+
+public class SynchronizedReceiverRunnable implements Runnable{
+	private String propertyPath; 
+	private ArrayList<String> errorLinks;
+
+	public SynchronizedReceiverRunnable(String propertyPath){
+		this.propertyPath = propertyPath;
+	}
+	
+	
+	
+	public void run() throws IllegalMonitorStateException{
+		boolean runInfinite = true;
+		
+		PropertiesReader property = new PropertiesReader(this.propertyPath);
+		
+		while(runInfinite){
+			
+			
+			Shimcalls call = new Shimcalls(property);
+			JSONArray allResearchObjects = call.getResearchObjectsList_repo();
+			
+			
+			for (Object item : allResearchObjects.toArray()){
+			
+			
+				JSONObject item_new = (JSONObject) item;
+				call.getObjectID(item_new, "Identifier");
+				String identifiter = call.getID();
+				String doi_url = null;
+				
+				if (identifiter == null){
+					System.err.println("[SDA Agent : Cannot get Identifier of RO]");
+				}else{
+					this.errorLinks = new ArrayList<String>();
+					String rootPath = null;
+					try{
+						JSONObject pulishObject = call.getResearchObject_cp(identifiter);
+						
+						call.getObjectID(pulishObject,"@id");
+						
+						String ore_url = call.getID();
+						
+						if (ore_url == null){
+							System.err.println("[SDA Agent : Cannot get ORE map file of RO");
+						}else{
+							
+							try{
+								JSONObject ore = call.getResearchObjectORE(ore_url);
+								
+								NewOREmap oreMap = new NewOREmap(ore);
+								JSONObject newOREmap = oreMap.getNewOREmap();
+								
+								try{
+									DummySDA dummySDA = new DummySDA(property, newOREmap);
+									errorLinks = dummySDA.getErrorLinks();
+									rootPath = dummySDA.getRootPath();
+									
+									try{
+										ZipDirectory zipDir = new ZipDirectory(rootPath);
+										
+										try{
+											SFTP sftp = new SFTP(this.propertyPath, rootPath+".zip");
+											String target = sftp.getFinalSource();
+											try{
+												DOI doi = new DOI(target, ore);
+												doi_url = doi.getDoi();
+											}catch(Exception e){
+												e.printStackTrace();
+												System.err.println("[SDA Agent : Cannot generate DOI]");
+											}
+											//call.updateStatus(doi_url, identifiter);
+											System.out.println(doi_url);
+											FileManager manager = new FileManager();
+											manager.removeTempFile(rootPath+".zip");
+											manager.removeTempFolder(rootPath);	
+										}catch(Exception e){
+											e.printStackTrace();
+											System.err.println("[SDA Agent: Cannot deposit RO zip file into SDA]");
+										}
+									}catch(Exception e){
+										e.printStackTrace();
+										System.err.println("[SDA Agent : Cannot compress downloaded RO folder]");
+									}
+								}catch(Exception e){
+									e.printStackTrace();
+									System.err.println("[SDA Agent : Cannot download RO into local server]");
+								}
+							}catch(Exception e){
+								e.printStackTrace();
+								System.err.println("[SDA Agent : Cannot access ORE link]");
+							}
+						}
+					}catch(Exception e){
+						e.printStackTrace();
+						System.err.println("[SDA Agent: Cannot access RO link]");
+					}
+				}
+			}
+					
+			
+			try {
+				Thread.sleep(10000);
+				System.out.println("Finish Sleeping");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+}
