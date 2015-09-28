82b013b1afa170568dd2469146bef3f50071d22d
charitha306 <charitha306@gmail.com>
2015-09-28 17:04:19 -0400

    Adding copyrights to classes in sead-api and sead-pdt

--- sead-pdt/src/main/java/org/seadpdt/RepoServices.java
+++ sead-pdt/src/main/java/org/seadpdt/RepoServices.java
@@ -1,7 +1,27 @@
+/*
+ *
+ * Copyright 2015 The Trustees of Indiana University, 2015 University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author myersjd@umich.edu
+ * @author smccaula@indiana.edu
+ */
+
 package org.seadpdt;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -11,7 +31,6 @@ import com.sun.jersey.api.client.ClientResponse.Status;
 import org.bson.Document;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.seadpdt.util.Constants;
 import org.seadpdt.util.MongoDB;
 
 import javax.ws.rs.*;
