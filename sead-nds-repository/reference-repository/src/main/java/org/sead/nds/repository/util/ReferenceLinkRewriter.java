dcdea4c231bdcb271df93ae5dbdc90bfb7b23a68
Jim Myers <myersjd@umich.edu>
2016-06-17 10:24:28 -0400

    fix/improve encoding in links

--- sead-nds-repository/reference-repository/src/main/java/org/sead/nds/repository/util/ReferenceLinkRewriter.java
+++ sead-nds-repository/reference-repository/src/main/java/org/sead/nds/repository/util/ReferenceLinkRewriter.java
@@ -26,9 +26,11 @@ public class ReferenceLinkRewriter implements LinkRewriter {
 			// /researchobjects/{id}/data/{relpath}
 			// relPath includes initial / which should not be encoded... so add
 			// that / to the '/data/' constant and skip first char of relPath
-			String start = encode(ro_id)+ "/data/"; 
-			return base + start
-					+ encode(relPath.substring(relPath.indexOf(start)+ (start.length()+1)));
+			String start = encode(ro_id) + "/data/";
+			return base
+					+ start
+					+ encode(relPath.substring(relPath.indexOf(start)
+							+ (start.length() + 1)));
 		} catch (URISyntaxException e) {
 			log.error(e.getLocalizedMessage());
 			return (sourceURL);
@@ -37,8 +39,7 @@ public class ReferenceLinkRewriter implements LinkRewriter {
 
 	public String rewriteOREMapLink(String sourceURL, String ro_id) {
 		try {
-			return base + encode(ro_id)
-					+ "/meta/oremap.jsonld.txt";
+			return base + encode(ro_id) + "/meta/oremap.jsonld.txt";
 		} catch (URISyntaxException e) {
 			log.error(e.getLocalizedMessage());
 			return (sourceURL);
@@ -47,15 +48,24 @@ public class ReferenceLinkRewriter implements LinkRewriter {
 
 	public String rewriteAggregationLink(String sourceURL, String ro_id) {
 		try {
-			return base + encode(ro_id)
-					+ "/meta/oremap.jsonld.txt#aggregation";
+			return base + encode(ro_id) + "/meta/oremap.jsonld.txt#aggregation";
 		} catch (URISyntaxException e) {
 			log.error(e.getLocalizedMessage());
 			return (sourceURL);
 		}
 	}
-	
+
+	// Need to percent encode bad chars that may be in file path names. Java
+	// doesn't appear to have a built in encode appropriate for path segments
+	// (URLEncoder encodes form data and doesn't do the right thing). For now,
+	// we'll catch the most common ones.
+	// See http://notes.richdougherty.com/2013/07/url-path-segment-encoding.html
+	// for other options
 	private String encode(String s) throws URISyntaxException {
-		return s.replaceAll("/",  "%2F");
+
+		return s.replaceAll("%", "%25").replaceAll("/", "%2F").replaceAll(" ", "%20")
+				.replaceAll("\\?", "%3F").replaceAll("#", "%23")
+				.replaceAll("\\[", "%5B").replaceAll("\\]", "%5D")
+				;
 	}
 }
